794372e2cf36c233e308854c2a8e8d1b877e8b58
tomaztk <tomaz.kastrun@gmail.com>
2020-04-26 15:59:34 +0200

    adding single list

---
+++ Essential_list_single_command.R
@@ -0,0 +1,75 @@
+#########################################
+####
+####  Essential R Packages for
+####  Data Scientists and Statisticians
+####
+####  Created by Tomaz Kastrun
+####  Date: April, 25, 2020
+####  Version: 0.0.1
+####
+####  ToDo: Selector for installing only sections
+#########################################
+
+
+install.packages(c("Hmisc","foreign","protr","readxl","xlsx",
+                   "csv","readr","tidyverse","jsonLite","rjson",
+                   "RJSONIO","jsonvalidate","sparkavro","arrow","feather",
+                   "XML","odbc","RODBC","mssqlR","RMySQL",
+                   "dbConnect","postGIStools","RPostgreSQL","ODBC","RRedshiftSQL",
+                   "RSQLite","sqliter","dbflobr","RSQL","sqldf",
+                   "poplite","queryparser","influxdbr","janitor","outliers",
+                   "missForest","frequency","Amelia","diffobj","mice",
+                   "VIM","Bioconductor","mi","wrangle","mitools",
+                   "stringr","lubridate","glue","scales","hablar",
+                   "dplyr","purr","magrittr","data.table","plyr",
+                   "tidyr","tibble","reshape2","stats","Lars",
+                   "caret","survival","gam","glmnet","quantreg",
+                   "sgd","BLR","MASS","car","mlogit",
+                   "earth","faraway","nortest","lmtest","nlme",
+                   "splines","sem","WLS","OLS","pls",
+                   "2SLS","3SLS","tree","rpart","rio",
+                   "FuzzyNumbers","ez","psych","CCA","CCP",
+                   "icapca","gvlma","smacof","MVN","rpca",
+                   "gpca","EFA.MRFA","MFAg","MVar","fabMix",
+                   "fad","spBFA","cate","mnlfa","CSFA",
+                   "GFA","lmds","SPCALDA","semds","superMDS",
+                   "vcd","vcdExtra","ks","rrcov","eRm",
+                   "MNP","bayesm","ltm","fpc","cluster",
+                   "treeClust","e1071","NbClust","skmeans","kml",
+                   "compHclust","protoclust","pvclust","genie","tclust",
+                   "ClusterR","dbscan","CEC","GMCM","EMCluster",
+                   "randomLCA","MOCCA","factoextra","poLCA","ts",
+                   "zoo","xts","timeSeries","tsModel","TSMining",
+                   "TSA","fma","fpp2","fpp3","tsfa",
+                   "TSdist","TSclust","feasts","MTS","dse",
+                   "sazedR","kza","fable","forecast","tseries",
+                   "nnfor","quantmod","fastnet","tsna","sna",
+                   "networkR","InteractiveIGraph","SemNeT","igraph","NetworkToolbox",
+                   "dyads","staTools","CINNA","tm","tau",
+                   "koRpus","lexicon","sylly","textir","textmineR",
+                   "MediaNews","lsa","ngram","ngramrr","corpustools",
+                   "udpipe","textstem","tidytext","text2vec","crossval",
+                   "bcv","klaR","EnsembleCV","gencve","cvAUC",
+                   "CVThresh","cvTools","dcv","cvms","blockCV",
+                   "randomForest","grf","ipred","party","randomForestSRC",
+                   "BART","Boruta","LTRCtrees","REEMtree","refr",
+                   "binomialRF","superml","gbm","GAMBoost","GMMBoost",
+                   "bst","sboost","C50","RWeka","klar",
+                   "kernlab","svmpath","nnet","gnn","rnn",
+                   "spnn","brnn","RSNNS","AMORE","simpleNeural",
+                   "ANN2","yap","yager","deep","neuralnet",
+                   "TeachNet","deepnet","RcppDL","tensorflow","h2o",
+                   "kerasR","deepNN","Buddle","automl","RLT",
+                   "ReinforcementLearning","MDPtoolbox","lime","localModel","iml",
+                   "EIX","flashlight","interpret","outliertree","breakDown",
+                   "dockerfiler","azuremlsdk","sparklyr","cloudml","ggvis",
+                   "htmlwidgets","maps","sunburstR","lattice","predict3d",
+                   "rgl","rglwidget","plot3Drgl","ggmap","ggplot2",
+                   "plotly","RColorBrewer","dygraphs","canvasXpress","qgraph",
+                   "moveVis","ggcharts","visNetwork","visreg","sjPlot",
+                   "plotKML","squash","statVisual","mlr3viz","DiagrammeR",
+                   "pavo","rasterVis","timelineR","DataViz","d3r",
+                   "d3heatmap","dashboard","highcharter","rbokeh","rvest",
+                   "Rcrawler","ralger","scrapeR","devtools","usethis",
+                   "roxygen2","knitr","rmarkdown","flexdashboard","Shiny",
+                   "xtable","httr","profvis"), dependencies = TRUE)
